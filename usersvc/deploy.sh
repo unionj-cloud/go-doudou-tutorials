6149d8de4eadaeb15d1a12b0f39e591b1adbd542
wubin1989 <328454505@qq.com>
2022-05-03 18:57:34 +0800

    ...

--- usersvc/deploy.sh
+++ usersvc/deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# 设置编译程序所需环境变量
 export GO111MODULE=on
 export GOPROXY=https://goproxy.cn,direct
-export TZ="Asia/Shanghai"
-
+# 编译程序，生成可执行文件
 go build -v -o api cmd/main.go
 
+# 启动mysql实例
+docker-compose -f docker-compose.yml up -d
+
+# 通过pm2启动服务进程
 pm2 reload ecosystem.config.js --only usersvc --env $1

9c960c9eb9a829eea2bf8392c0b8d07c46c5d081
wubin1989 <328454505@qq.com>
2022-05-03 19:02:01 +0800

    ...

--- usersvc/deploy.sh
+++ usersvc/deploy.sh
@@ -9,5 +9,8 @@ go build -v -o api cmd/main.go
 # 启动mysql实例
 docker-compose -f docker-compose.yml up -d
 
+# 等待mysql实例启动完毕，可以连接
+sleep 15s
+
 # 通过pm2启动服务进程
 pm2 reload ecosystem.config.js --only usersvc --env $1

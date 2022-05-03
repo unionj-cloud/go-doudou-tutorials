554d64410ba806b62ef3aae7bcef066556afd350
wubin1989 <328454505@qq.com>
2022-05-03 18:39:02 +0800

    ...

--- usersvc/deploy.sh
+++ usersvc/deploy.sh
@@ -4,7 +4,7 @@ export GO111MODULE=on
 export GOPROXY=https://goproxy.cn,direct
 export TZ="Asia/Shanghai"
 
-go env
+echo $PWD
 go build -v -o api cmd/main.go
 
 pm2 reload ecosystem.config.js --only usersvc --env $1

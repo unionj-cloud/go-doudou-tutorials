c863d380862ac46934cfc29bc6efe00996a4dfde
wubin1989 <328454505@qq.com>
2022-05-03 18:36:26 +0800

    ...

--- usersvc/ecosystem.config.js
+++ usersvc/ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
   "apps": [
     {
       "name": "usersvc",
-      "script": "export TZ=\"Asia/Shanghai\" && ./api",
+      "script": "./api",
       "cwd": "/root/deploy/go-doudou-tutorials/usersvc",
       "exec_interpreter": "",
       "exec_mode": "fork",

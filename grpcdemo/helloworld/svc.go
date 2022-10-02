2933b8fbf994aaecd1d55d65d649d970b6c9449e
wubin1989 <328454505@qq.com>
2022-10-02 21:04:25 +0800

    ...

--- grpcdemo/helloworld/svc.go
+++ grpcdemo/helloworld/svc.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"github.com/unionj-cloud/helloworld/vo"
 )
 
 //go:generate go-doudou svc http --handler --doc -c
@@ -10,19 +11,23 @@ import (
 type Helloworld interface {
 	// Greeting 问候接口
 	Greeting(ctx context.Context,
-		// 入参
+	// 入参
 		greeting string) (
-		// 出参
+	// 出参
 		data string,
-		// 错误信息
+	// 错误信息
 		err error)
 
 	// Bye 再见接口
 	Bye(ctx context.Context,
-		// 入参
+	// 入参
 		greeting string) (
-		// 出参
+	// 出参
 		data string,
-		// 错误信息
+	// 错误信息
 		err error)
+
+	BiStream(ctx context.Context, stream vo.Order) (stream1 vo.Page, err error)
+	ClientStream(ctx context.Context, stream vo.Order) (data vo.Page, err error)
+	ServerStream(ctx context.Context, payload vo.Order) (stream vo.Page, err error)
 }
